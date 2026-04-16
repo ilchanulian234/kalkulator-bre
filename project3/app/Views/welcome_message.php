3afcf5594b884a581e888bafce64c11924812dc4
Ilchanul Yaasin <ilchanulian234@gmail.com>
2026-04-16 21:09:58 +0700

    menambah file project3

--- project3/app/Views/welcome_message.php
+++ project3/app/Views/welcome_message.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <h1>Halo Dunia</h1>
     <title>Welcome to CodeIgniter 4!</title>
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
