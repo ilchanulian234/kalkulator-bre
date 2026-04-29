01828c96f8e6b16c3b85c1ad4d2cbfcd9c97f576
Ilchanul Yaasin <ilchanulian234@gmail.com>
2026-04-29 19:46:53 +0700

    sudah login-logout

--- project3/app/Controllers/BaseController.php
+++ project3/app/Controllers/BaseController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 use CodeIgniter\Controller;
+use CodeIgniter\HTTP\CLIRequest;
+use CodeIgniter\HTTP\IncomingRequest;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use Psr\Log\LoggerInterface;
@@ -12,19 +14,33 @@ use Psr\Log\LoggerInterface;
  * and performing functions that are needed by all your controllers.
  *
  * Extend this class in any new controllers:
- * ```
  *     class Home extends BaseController
- * ```
  *
  * For security, be sure to declare any new methods as protected or private.
  */
 abstract class BaseController extends Controller
 {
     /**
+     * Instance of the main Request object.
+     *
+     * @var CLIRequest|IncomingRequest
+     */
+    protected $request;
+
+    /**
+     * An array of helpers to be loaded automatically upon
+     * class instantiation. These helpers will be available
+     * to all other controllers that extend BaseController.
+     *
+     * @var array
+     */
+    // Update baris ke-38: Menambahkan 'auth' secara global
+    protected $helpers = ['auth', 'url', 'form'];
+
+    /**
      * Be sure to declare properties for any property fetch you initialized.
      * The creation of dynamic property is deprecated in PHP 8.2.
      */
-
     // protected $session;
 
     /**
@@ -32,11 +48,7 @@ abstract class BaseController extends Controller
      */
     public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
     {
-        // Load here all helpers you want to be available in your controllers that extend BaseController.
-        // Caution: Do not put the this below the parent::initController() call below.
-        // $this->helpers = ['form', 'url'];
-
-        // Caution: Do not edit this line.
+        // Do Not Edit This Line
         parent::initController($request, $response, $logger);
 
         // Preload any models, libraries, etc, here.
